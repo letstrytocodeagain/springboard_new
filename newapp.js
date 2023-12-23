05cb849ebf9a243a2c4cd917a95bc490254827b5
Diego M <guerrero.barracks@gmail.com>
2023-12-22 21:16:17 -0500

    fixing small issues

--- newapp.js
+++ newapp.js
@@ -1,9 +1,2 @@
 console.log("hello world");
-<<<<<<< HEAD
 console.log("hello world");
-=======
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello");
->>>>>>> creating_conflict
